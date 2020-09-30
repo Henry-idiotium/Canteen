17445d47b752aa88f99a40823a16b7023417f0a2
kolo4565656 <leminhschool@gmail.com>
2020-09-30 07:28:33 +0700

    a

--- Canteen/admin/autoload/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-  session_start();
-  require_once __dir__. "/../../library/Database.php";
-  require_once __dir__. "/../../library/Function.php";
-  $db = new Database
-?>
--- Canteen/public/admin/js/myjs.js
+++
@@ -1,4 +0,0 @@
-
-$(".alert").fadeTo(2000, 500).slideUp(500, function(){
-    $(".alert").slideUp(500);
-});
