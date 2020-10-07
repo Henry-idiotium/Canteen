6321c3578d6296456d63689e22cff4801545ecf1
henry_monad <quocdat.381872@gmail.com>
2020-10-07 10:54:34 +0700

    exterminate

--- Canteen/admin/autoload/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    session_start();
-    require_once __dir__. "/../../library/Database.php";
-    require_once __dir__. "/../../library/Function.php";
-    $db = new Database;
-    define('ROOT', $_SERVER['DOCUMENT_ROOT']."/Canteen/public/uploads/");
-    
-?>
