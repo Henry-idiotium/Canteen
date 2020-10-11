baa0b9b74205a3f25ff77e4499cdf743a2854dbc
henry_monad <quocdat.381872@gmail.com>
2020-10-11 09:40:17 +0700

    script replacement

--- admin/layouts/footer.php
+++ admin/layouts/footer.php
@@ -25,21 +25,6 @@
                     </div>
                 </div>
             </div>
-
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-            <!-- Bootstrap core JavaScript-->
-            <script src="<?php echo base_url() ?>public/admin/js/jquery.min.js"></script>
-            <script src="<?php echo base_url() ?>public/admin/js/bootstrap.bundle.min.js"></script>
-            <!-- Core plugin JavaScript-->
-            <script src="<?php echo base_url() ?>public/admin/js/jquery.easing.min.js"></script>
-            <!-- Custom scripts for all pages-->
-            <script src="<?php echo base_url() ?>public/admin/js/sb-admin-2.min.js"></script>
-            <!-- Page level plugins -->
-            <script src="<?php echo base_url() ?>public/admin/js/Chart.min.js"></script>
-            <!-- Page level custom scripts -->
-            <script src="<?php echo base_url() ?>public/admin/js/demo/chart-area-demo.js"></script>
-            <script src="<?php echo base_url() ?>public/admin/js/demo/chart-pie-demo.js"></script>
-            <script src="<?php echo base_url() ?>public/admin/js/myjs.js"></script>
         </div>
         <!-- End of Page Wrapper -->
         
@@ -114,5 +99,20 @@
             </div>
         </footer>
         <!-- End of Footer -->
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!-- Bootstrap core JavaScript-->
+        <script src="<?php echo base_url() ?>public/admin/js/jquery.min.js"></script>
+        <script src="<?php echo base_url() ?>public/admin/js/bootstrap.bundle.min.js"></script>
+        <!-- Core plugin JavaScript-->
+        <script src="<?php echo base_url() ?>public/admin/js/jquery.easing.min.js"></script>
+        <!-- Custom scripts for all pages-->
+        <script src="<?php echo base_url() ?>public/admin/js/sb-admin-2.min.js"></script>
+        <!-- Page level plugins -->
+        <script src="<?php echo base_url() ?>public/admin/js/Chart.min.js"></script>
+        <!-- Page level custom scripts -->
+        <script src="<?php echo base_url() ?>public/admin/js/demo/chart-area-demo.js"></script>
+        <script src="<?php echo base_url() ?>public/admin/js/demo/chart-pie-demo.js"></script>
+        <script src="<?php echo base_url() ?>public/admin/js/myjs.js"></script>
     </body>
 </html>
